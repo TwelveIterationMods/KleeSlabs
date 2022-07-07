df73540e57ee8c54a52e9c2c10b5c1f144c1b0ec
CebbyS <cebbys.dev@gmail.com>
2022-07-07 14:29:52 +0300

    SlabConverter - Added a new method to the SlabConverter interface for compatibility fix
    BlockBreakHandler
    - Vector hit position calculated before asking for single slab state
    - By doing so correct slab is used for block state property calculation
    - Changed SlabConverter method calls to the new method

--- shared/src/main/java/net/blay09/mods/kleeslabs/converter/SlabConverter.java
+++ shared/src/main/java/net/blay09/mods/kleeslabs/converter/SlabConverter.java
@@ -1,11 +1,18 @@
+
+
 package net.blay09.mods.kleeslabs.converter;
 
+import net.blay09.mods.balm.api.event.BreakBlockEvent;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.SlabType;
 
 public interface SlabConverter {
 
     BlockState getSingleSlab(BlockState state, SlabType slabType);
+
+    default BlockState getSingleSlab(BreakBlockEvent event, SlabType slabType) {
+        return getSingleSlab(event.getState(), slabType);
+    }
 
     default boolean isDoubleSlab(BlockState state) {
         return true;
