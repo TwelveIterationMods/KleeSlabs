56769d946eebfbfdbc5235b795162ea4810fc5f4
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-10-10 19:18:37 +0200

    feat: Update to Minecraft 1.21.2

--- common/src/main/java/net/blay09/mods/kleeslabs/client/KleeSlabsClient.java
+++ common/src/main/java/net/blay09/mods/kleeslabs/client/KleeSlabsClient.java
@@ -3,17 +3,16 @@ package net.blay09.mods.kleeslabs.client;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.blay09.mods.balm.api.Balm;
-import net.blay09.mods.balm.api.client.BalmClient;
 import net.blay09.mods.balm.api.event.client.BlockHighlightDrawEvent;
 import net.blay09.mods.kleeslabs.KleeSlabs;
 import net.blay09.mods.kleeslabs.converter.SlabConverter;
-import net.blay09.mods.kleeslabs.mixin.LevelRendererAccessor;
 import net.blay09.mods.kleeslabs.registry.SlabRegistry;
 import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.ShapeRenderer;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
@@ -57,7 +56,7 @@ public class KleeSlabsClient {
             double camX = camera.getPosition().x;
             double camY = camera.getPosition().y;
             double camZ = camera.getPosition().z;
-            LevelRendererAccessor.callRenderShape(poseStack, vertexBuilder, shape, -camX, -camY, -camZ, 0f, 0f, 0f, 0.4f);
+            ShapeRenderer.renderShape(poseStack, vertexBuilder, shape, -camX, -camY, -camZ, 0x66000000);
 
             event.setCanceled(true);
         }
