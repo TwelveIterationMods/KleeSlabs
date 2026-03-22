accbb69a6183955be6b7170b053fe501bf817a58
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-03-22 14:54:48 +0100

    build: Update to Minecraft 26.1

--- fabric/src/main/java/net/blay09/mods/kleeslabs/fabric/datagen/ModBlockTagProvider.java
+++ fabric/src/main/java/net/blay09/mods/kleeslabs/fabric/datagen/ModBlockTagProvider.java
@@ -1,8 +1,6 @@
 package net.blay09.mods.kleeslabs.fabric.datagen;
 
 import net.blay09.mods.kleeslabs.tag.ModBlockTags;
-import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
