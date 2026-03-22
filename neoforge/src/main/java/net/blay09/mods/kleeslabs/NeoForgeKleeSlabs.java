9c4d543bcfc77810107d24f8c090f4777821e9ed
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-03-22 16:23:24 +0100

    feat: Update to Minecraft 26.1

--- neoforge/src/main/java/net/blay09/mods/kleeslabs/NeoForgeKleeSlabs.java
+++ neoforge/src/main/java/net/blay09/mods/kleeslabs/NeoForgeKleeSlabs.java
@@ -3,13 +3,14 @@ package net.blay09.mods.kleeslabs;
 import net.blay09.mods.balm.Balm;
 import net.blay09.mods.balm.neoforge.platform.runtime.NeoForgeLoadContext;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 
 @Mod(KleeSlabs.MOD_ID)
 public class NeoForgeKleeSlabs {
 
-    public NeoForgeKleeSlabs(IEventBus eventBus) {
-        final var context = new NeoForgeLoadContext(eventBus);
+    public NeoForgeKleeSlabs(ModContainer modContainer, IEventBus eventBus) {
+        final var context = new NeoForgeLoadContext(modContainer, eventBus);
         Balm.initializeMod(KleeSlabs.MOD_ID, context, KleeSlabs::initialize);
     }
 
--- neoforge/src/main/java/net/blay09/mods/kleeslabs/client/NeoForgeKleeSlabsClient.java
+++ neoforge/src/main/java/net/blay09/mods/kleeslabs/client/NeoForgeKleeSlabsClient.java
@@ -5,13 +5,14 @@ import net.blay09.mods.balm.neoforge.platform.runtime.NeoForgeLoadContext;
 import net.blay09.mods.kleeslabs.KleeSlabs;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 
 @Mod(value = KleeSlabs.MOD_ID, dist = Dist.CLIENT)
 public class NeoForgeKleeSlabsClient {
 
-    public NeoForgeKleeSlabsClient(IEventBus eventBus) {
-        final var context = new NeoForgeLoadContext(eventBus);
+    public NeoForgeKleeSlabsClient(ModContainer modContainer, IEventBus eventBus) {
+        final var context = new NeoForgeLoadContext(modContainer, eventBus);
         BalmClient.initializeMod(KleeSlabs.MOD_ID, context, KleeSlabsClient::initialize);
     }
 
