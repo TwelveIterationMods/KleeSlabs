72a65564ba7f72ce0ad54d8bf78f2784a16852e0
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-04-21 20:24:37 +0200

    fix: Fix config i18n keys

--- common/src/main/java/net/blay09/mods/kleeslabs/KleeSlabsConfig.java
+++ common/src/main/java/net/blay09/mods/kleeslabs/KleeSlabsConfig.java
@@ -1,6 +1,7 @@
 package net.blay09.mods.kleeslabs;
 
 import net.blay09.mods.balm.api.Balm;
+import net.blay09.mods.balm.common.config.ConfigLocalization;
 
 public class KleeSlabsConfig {
 
@@ -9,6 +10,7 @@ public class KleeSlabsConfig {
     }
 
     public static void initialize() {
+        ConfigLocalization.enableModernTranslationKeys(KleeSlabs.MOD_ID);
         Balm.getConfig().registerConfig(KleeSlabsConfigData.class, null);
     }
 
