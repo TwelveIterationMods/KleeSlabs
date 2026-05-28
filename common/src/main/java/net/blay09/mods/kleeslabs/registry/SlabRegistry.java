fb20a3a8f091e965f817eea0e873c12f39475f82
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-05-28 17:00:10 +0200

    feat: Add support for tags to map to slab converters

--- common/src/main/java/net/blay09/mods/kleeslabs/registry/SlabRegistry.java
+++ common/src/main/java/net/blay09/mods/kleeslabs/registry/SlabRegistry.java
@@ -2,22 +2,30 @@ package net.blay09.mods.kleeslabs.registry;
 
 import net.blay09.mods.kleeslabs.KleeSlabs;
 import net.blay09.mods.kleeslabs.converter.SlabConverter;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public class SlabRegistry {
     private static final List<SlabRegistryData> slabRegistry = new ArrayList<>();
     private static final Map<Block, SlabConverter> slabMap = new HashMap<>();
+    private static final Map<TagKey<Block>, SlabConverter> tagMap = new LinkedHashMap<>();
 
     public static void registerSlabConverter(Block doubleSlab, SlabConverter converter) {
         slabMap.put(doubleSlab, converter);
+    }
+
+    public static void registerSlabConverter(TagKey<Block> tag, SlabConverter converter) {
+        tagMap.put(tag, converter);
     }
 
     public static void registerSlab(SlabRegistryData data) {
@@ -40,6 +48,22 @@ public class SlabRegistry {
     @Nullable
     public static SlabConverter getSlabConverter(Block block) {
         return slabMap.get(block);
+    }
+
+    @Nullable
+    public static SlabConverter getSlabConverter(BlockState state) {
+        final var blockConverter = slabMap.get(state.getBlock());
+        if (blockConverter != null) {
+            return blockConverter;
+        }
+
+        for (final var entry : tagMap.entrySet()) {
+            if (state.is(entry.getKey())) {
+                return entry.getValue();
+            }
+        }
+
+        return null;
     }
 
     public static void clearRegistry() {
